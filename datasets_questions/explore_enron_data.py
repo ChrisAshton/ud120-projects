cd6de3f0a7277c53b924cfce06330710c9fd60a8
ChrisAshton <christopherashton@g.harvard.edu>
2016-12-21 22:22:21 -0500

    playing around with explore_enron_data.py ...still learning

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,24 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
+#import pandas
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+#df = pandas.DataFrame(enron_data)
+
+print enron_data
+max = 0
+count = 0
+
+for i in enron_data:
+    count = len(enron_data[i])
+    if count > max:
+        max = count
+
+print max 
